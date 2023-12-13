fcd7d9eea14ccaa025989fd4d9687643450d56a5
Ibraheem Khan <ikhan@college.harvard.edu>
2023-12-13 11:29:01 -0600

    Update creditCard.py

--- creditCard.py
+++ creditCard.py
@@ -1,28 +1,36 @@
-
 class CreditCard:
 
     def __init__(self, limit):
-        self.av_limit = limit
-        self.balance = 0
-        self.pending_transactions = {}
-        self.pending_payments = {}
+        # Initializing the credit card with the specified limit, balance, and pending transactions/payments.
+        self.av_limit = limit  # Available credit limit
+        self.balance = 0  # Current balance
+        self.pending_transactions = {}  # Dictionary to store pending transaction IDs and amounts
+        self.pending_payments = {}  # Dictionary to store pending payment IDs and amounts
 
     def __repr__(self):
+        # Representation of the Credit Card object
         return f'Credit Card - ${self.av_limit} credit limit'
 
     ### TRANSACTIONS ###
+
     def authorize(self, id, amt):
+        # Authorize a transaction by reserving credit for a pending transaction.
+        # Updates available credit and adds the transaction to pending_transactions.
         if amt <= self.av_limit:
             self.av_limit -= amt
-            self.pending_transactions[(id,'payment')] = amt
+            self.pending_transactions[(id, 'payment')] = amt
 
     def settle(self, id, extra=0):
+        # Settle a pending transaction, updating available credit and balance.
+        # Finalizes the transaction, adds any extra fees, and removes it from pending_transactions.
         if id in self.pending_transactions:
             self.av_limit -= extra
             self.balance += self.pending_transactions[id] + extra
             del self.pending_transactions[id]
 
     def clearTransaction(self, id):
+        # Cancel a pending transaction and reclaim reserved credit.
+        # Reverts the available credit by adding the pending transaction amount.
         if id in self.pending_transactions:
             self.av_limit += self.pending_transactions[id]
             del self.pending_transactions[id]
@@ -30,41 +38,21 @@ class CreditCard:
     ### PAYMENTS ###
 
     def initiate_payment(self, id, amt):
+        # Initiate a payment by reducing the balance by a specified amount (marked as pending).
         self.balance -= amt
         self.pending_payments[id] = amt
 
-    def post_payment(self,id):
+    def post_payment(self, id):
+        # Finalize a pending payment, increasing available credit.
+        # Updates available credit by adding the pending payment amount.
         if id in self.pending_payments:
-            self.limit += self.pending_payments[id]
+            self.av_limit += self.pending_payments[id]
             del self.pending_payments[id]
 
     def clearPayment(self, id):
+        # Reverse a pending payment and adjust the balance accordingly.
+        # Reverts the balance by adding the pending payment amount.
         if id in self.pending_payments:
-            self.bal += self.pending_payments[id]
+            self.balance += self.pending_payments[id]
             del self.pending_payments[id]
 
-
-
-
-
-
-c = CreditCard(1000)
-
-print(c.av_limit)
-print(c.balance)
-
-c.authorize(1,200)
-print(c.av_limit)
-print(c.balance)
-
-c.settle(1,5)
-print(c.av_limit)
-print(c.balance)
-
-c.authorize(2,-100)
-print(c.av_limit)
-print(c.balance)
-
-c.settle(2)
-print(c.av_limit)
-print(c.balance)
